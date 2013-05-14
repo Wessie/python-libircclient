9de19feaff87ec6585827dacdbbfcb6181d2fdae
wessie <wessie@wessie.info>
2013-05-15 00:19:11 +0200

    Barebone README and setup.py added.

---
+++ README.md
@@ -0,0 +1,1 @@
+A Python CFFI wrapper around the C libircclient found on sourceforge.
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name="python-libircclient",
+    version="0.1",
+    url="http://github.com/Wessie/python-libircclient",
+    author="Wesley Bitter",
+    author_email="pylibircclient@wessie.info",
+    description="Python CFFI wrapper around libircclient.",
+    zip_safe=False,
+    platforms='any',
+    packages="libircclient"
+)
